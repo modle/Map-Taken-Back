31e0b2388119fb25d3fda94195da47c60d6595d6
Matthew Odle <odle.matthew@gmail.com>
2014-12-16 12:53:13 -0600

    Update dbCreate.sql

--- dbCreate.sql
+++ dbCreate.sql
@@ -18,3 +18,14 @@ CREATE TABLE IF NOT EXISTS `dualblades` (
   `Defense` varchar(6) NOT NULL,
   PRIMARY KEY (`Id`)
 );
+
+INSERT INTO `dualblades` (`Id`, `Name`, `Own`, `Attack`, `Elem_Value`, `Element`, `Elem_Value_two`, `Element_two`, `Sharpness`, `Slot`, `Rare`, `Affinity`, `Defense`) VALUES
+(1, 'Agnaktor InfernoFINAL', 0, 322, 340, 'FIR', 0, '', '', 0, 9, '0.15', ''),
+(2, 'Alkaid''s AsterismFINAL', 0, 350, 180, 'POI', 0, '', '', 0, 10, '0.20', ''),
+(3, 'Alluvion SlashersFINAL', 0, 336, 420, 'WAT', 0, '', '', 1, 9, '0.00', ''),
+(4, 'Blizzarioths', 0, 266, 180, 'ICE', 0, '', '', 0, 5, '0.10', ''),
+(5, 'Blizzarioths+', 0, 294, 200, 'ICE', 0, '', '', 0, 6, '0.20', ''),
+(6, 'Bloodwings', 0, 182, 160, 'POI', 0, '', '', 0, 3, '0.00', ''),
+(7, 'Bloodwings+', 0, 196, 200, 'POI', 0, '', '', 0, 3, '0.00', ''),
+(8, 'Boltgeist', 0, 252, 200, 'THU', 0, '', '', 1, 5, '0.00', ''),
+(9, 'Bone Scythes+', 0, 126, 100, '(POI)', 0, '', '', 0, 1, '0.00', '');
