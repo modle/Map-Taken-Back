c12b02c8a612c5adbba3cf022e266f77a61d27a0
Matthew Odle <odle.matthew@gmail.com>
2014-12-16 12:35:11 -0600

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,7 @@
+<?php
+    $db_name = "mh3u_app";
+    $un = "root"; //may need root with blank password, depending on setup; real server will be set up with actual username and password; mysql wants "", mysqli wants "root"; refer to mysql.user table
+    $pw = "";
+    $host="localhost";
+    $mysqli = new mysqli($host, $un, $pw, $db_name) or die (mysqli_error($mysqli));
+?>
