f27ee3399f01ae3f701e3e5a73e5f51aeeb0a0d5
modle13 <odle.matthew@gmail.com>
2015-04-19 23:42:12 -0500

    asdf

--- assets/modules/general/db_connect.php
+++ assets/modules/general/db_connect.php
@@ -2,7 +2,6 @@
     $db_name = "mh4u_app";
     $un = "root"; //may need root with blank password, depending on setup; real server will be set up with actual username and password; mysql wants "", mysqli wants "root"; refer to mysql.user table
     $pw = "";
-    //$pw = "kristi12";
     $host="127.0.0.1";
     $mysqli = new mysqli($host, $un, $pw, $db_name) or die (mysqli_error($mysqli));
 ?>
