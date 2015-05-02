3622468a027022288bf1e512cb4201f17980a908
modle13 <odle.matthew@gmail.com>
2015-05-01 22:06:13 -0500

    db update
    db update
    missed the sync of inputs.php in previous push

--- assets/modules/weapons/inputs.php
+++ assets/modules/weapons/inputs.php
@@ -1,80 +1,81 @@
 <!--dropdown, search box, create/final/awaken toggles-->
-
-<?php echo($weaponDropdownString) ?>
-<br>
-<input type='text' placeholder='Weapon Name' value='<?php echo($weaponSearch) ?>' name='weaponName' />
-<table class='nav'>
+<table class='nav' style='float: left'>
     <tr>
-        <th class='navTdTh'>[Create]</th>
-        <th class='navTdTh'>[Final]</th>
-        <th class='navTdTh'>[Awaken]</th>
-    </tr>
-    <tr>
-        <td class='navTdTh'><input type='checkbox' value='1' <?php echo($createCheck) ?> name='createShow' onchange='this.form.submit()' class='checkbox'/>
-        <td class='navTdTh'><input type='checkbox' value='1' <?php echo($finalCheck) ?> name='finalShow' onchange='this.form.submit()' class='checkbox'/>
-        <td class='navTdTh'><input type='checkbox' value='1' <?php echo($awakenCheck) ?> name='awakenShow' onchange='this.form.submit()' class='checkbox'/>
-    </tr>
-</table>
-
-<!--//rarity sliders-->
-Rarity
-<br>
-Min:
-    <input type='range' min=1 max=10 value='<?php echo($minRaritySelect) ?>' step=1.0 id='minRange' name='minRaritySelect' onchange='updateRarityMin(this.value, maxRaritySelect.value)' class='range' >
-    <input type='text' id='rarityMin' readonly size=1 value='<?php echo($minRaritySelect) ?>' style='background-color: rgba(0,0,0,0); border: none;'>
-
-<br>
-Max:
-    <input type='range' min=1 max=10 value='<?php echo($maxRaritySelect) ?>' step=1.0 id='maxRange' name='maxRaritySelect' onchange='updateRarityMax(minRaritySelect.value, this.value)' class='range' >
-    <input type='text' id='rarityMax' readonly size=1 value='<?php echo($maxRaritySelect) ?>' style='background-color: rgba(0,0,0,0); border: none;'>
-<br>
-
-<!--//Element Radios-->
-
-<!--//All and raw-->
-<table class='nav'>
-    <tr>
-        <td class='navTdTh'><input type='radio' id='all' value='%' name='elem' onchange='this.form.submit();' <?php echo($allCheck) ?>>
-        ALL |
-        <td class='navTdTh'><input type='radio' id='raw' value='RAW' name='elem' onchange='this.form.submit()' <?php echo($rawCheck) ?>>
-        <img src=assets/resources/elements/RAW.png class='icon'>
-    </tr>
-</table>
-
-<!--//elements-->
-<table class='nav'>
-    <tr><td class='navTdTh'><input type='radio' id='fire' value='FIR' name='elem' onchange='this.form.submit()' <?php echo($firCheck) ?>>
-        <img src=assets/resources/elements/FIR.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='water' value='WAT' name='elem' onchange='this.form.submit()' <?php echo($watCheck) ?>>
-        <img src=assets/resources/elements/WAT.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='thunder' value='THU' name='elem' onchange='this.form.submit()' <?php echo($thuCheck) ?>>
-        <img src=assets/resources/elements/THU.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='ice' value='ICE' name='elem' onchange='this.form.submit()' <?php echo($iceCheck) ?>>
-        <img src=assets/resources/elements/ICE.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='dragon' value='DRA' name='elem' onchange='this.form.submit()' <?php echo($draCheck) ?>>
-        <img src=assets/resources/elements/DRA.png class='icon'>
-    </tr>
-</table>
-
-<!--//status effects-->
-<table class='nav'>
-    <tr><td class='navTdTh'><input type='radio' id='paralysis' value='PAR' name='elem' onchange='this.form.submit()' <?php echo($parCheck) ?>>
-        <img src=assets/resources/elements/PAR.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='poison' value='POI' name='elem' onchange='this.form.submit()' <?php echo($poiCheck) ?>>
-        <img src=assets/resources/elements/POI.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='sleep' value='SLE' name='elem' onchange='this.form.submit()' <?php echo($sleCheck) ?>>
-        <img src=assets/resources/elements/SLE.png class='icon'>
-         |
-        <td class='navTdTh'><input type='radio' id='blast' value='BLA' name='elem' onchange='this.form.submit()' <?php echo($blaCheck) ?>>
-        <img src=assets/resources/elements/BLA.png class='icon'>
         <td class='navTdTh'>
+            <input type='submit' value='Reset' name='ResetButton' />
     </tr>
 </table>
+<br><br>
+<div style='clear:both'></div>
+<table class='nav' width='100%'>
+    <tr>
+        <td class='navTdTh' col width='80%'>
+            <?php echo($weaponDropdownString) ?>
+            <br><br>
+            <input type='text' placeholder='Weapon Name' value='<?php echo($weaponSearch) ?>' name='weaponName'/>
+            <br><br>
+            <table class='nav'>
+                <tr>
+                    <td class='navTdTh' style='text-align: right;'>
+                        <b>Create</b><br>
+                        <b>Final</b><br>
+                        <b>Awaken</b>
+                    <td class='navTdTh'>
+                        <input type='checkbox' value='1' <?php echo($createCheck) ?> name='createShow' onchange='this.form.submit()'    class= 'checkbox'/><br>
+                        <input type='checkbox' value='1' <?php echo($finalCheck) ?> name='finalShow' onchange='this.form.submit()' class='checkbox'/><br>
+                        <input type='checkbox' value='1' <?php echo($awakenCheck) ?> name='awakenShow' onchange='this.form.submit()' class='checkbox'/>
+                </tr>
+            </table>
+            <br><br>
+            <b>Rarity</b>
+            <br>
+            Min:
+            <input type='range' min=1 max=10 value='<?php echo($minRaritySelect) ?>' step=1.0 id='minRange' name='minRaritySelect' onchange='updateRarityMin(this.value, maxRaritySelect.value)' class='range' >
+            <input type='text' id='rarityMin' readonly size=1 value='<?php echo($minRaritySelect) ?>' style='background-color: rgba(0,0,0,0); border: none;'>
+            <br>
+            Max:
+            <input type='range' min=1 max=10 value='<?php echo($maxRaritySelect) ?>' step=1.0 id='maxRange' name='maxRaritySelect' onchange='updateRarityMax(minRaritySelect.value, this.value)' class='range' >
+            <input type='text' id='rarityMax' readonly size=1 value='<?php echo($maxRaritySelect) ?>' style='background-color: rgba(0,0,0,0); border: none;'>
+            
+        <!--//Element Radios-->
+        <td class='navTdTh' col width='50%'>
+            <input type='radio' id='all' value='%' name='elem' onchange='this.form.submit();' <?php echo($allCheck) ?>>
+            All
+            <br>
+            <input type='radio' id='raw' value='RAW' name='elem' onchange='this.form.submit()' <?php echo($rawCheck) ?>>
+            <img src=assets/resources/elements/RAW.png class='icon'>
+            <br>
 
+            <!--//elements-->
+            <input type='radio' id='fire' value='FIR' name='elem' onchange='this.form.submit()' <?php echo($firCheck) ?>>
+            <img src=assets/resources/elements/FIR.png class='icon'>
+            <br>
+            <input type='radio' id='water' value='WAT' name='elem' onchange='this.form.submit()' <?php echo($watCheck) ?>>
+            <img src=assets/resources/elements/WAT.png class='icon'>
+            <br>
+            <input type='radio' id='thunder' value='THU' name='elem' onchange='this.form.submit()' <?php echo($thuCheck) ?>>
+            <img src=assets/resources/elements/THU.png class='icon'>
+            <br>
+            <input type='radio' id='ice' value='ICE' name='elem' onchange='this.form.submit()' <?php echo($iceCheck) ?>>
+            <img src=assets/resources/elements/ICE.png class='icon'>
+            <br>
+            <input type='radio' id='dragon' value='DRA' name='elem' onchange='this.form.submit()' <?php echo($draCheck) ?>>
+            <img src=assets/resources/elements/DRA.png class='icon'>
+            <br>
+            
+            <!--//status effects-->
+            <input type='radio' id='paralysis' value='PAR' name='elem' onchange='this.form.submit()' <?php echo($parCheck) ?>>
+            <img src=assets/resources/elements/PAR.png class='icon'>
+            <br>
+            <input type='radio' id='poison' value='POI' name='elem' onchange='this.form.submit()' <?php echo($poiCheck) ?>>
+            <img src=assets/resources/elements/POI.png class='icon'>
+            <br>
+            <input type='radio' id='sleep' value='SLE' name='elem' onchange='this.form.submit()' <?php echo($sleCheck) ?>>
+            <img src=assets/resources/elements/SLE.png class='icon'>
+            <br>
+            <input type='radio' id='blast' value='BLA' name='elem' onchange='this.form.submit()' <?php echo($blaCheck) ?>>
+            <img src=assets/resources/elements/BLA.png class='icon'>
+            <br>
+    </tr>
+</table>
 <br>
--- assets/modules/weapons/query.php
+++ assets/modules/weapons/query.php
@@ -1,17 +1,13 @@
 <?php
     $weaponSearch=str_replace('\'','&#39;',$weaponSearch);
-
-    //if($awaken)
-        $sql = 'SELECT *
-            FROM weapondata
-            WHERE instr(name,"' . $weaponSearch . '")>0
-            AND rare BETWEEN ' . $minRaritySelect . ' AND ' . $maxRaritySelect . '
-            AND element LIKE \'%' . $elemFilter . '%\'
-            AND (' . $createFilter . '=0 OR created=' . $createFilter .')
-            AND (' . $finalFilter . '=0 OR final=' . $finalFilter . ')
-            AND (0=' . $weaponType . ' OR weapontypeid=' . $weaponType . ')
-            ORDER BY weaponId';
-
-
+    $sql = 'SELECT *
+        FROM weapondata
+        WHERE instr(name,"' . $weaponSearch . '")>0
+        AND rare BETWEEN ' . $minRaritySelect . ' AND ' . $maxRaritySelect . '
+        AND element LIKE \'%' . $elemFilter . '%\'
+        AND (' . $createFilter . '=0 OR created=' . $createFilter .')
+        AND (' . $finalFilter . '=0 OR final=' . $finalFilter . ')
+        AND (0=' . $weaponType . ' OR weapontypeid=' . $weaponType . ')
+        ORDER BY weaponId';
     $result = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli) . '; weapon table error');
 ?>
